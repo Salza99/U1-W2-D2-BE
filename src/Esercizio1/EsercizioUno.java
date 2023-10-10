45bbd21689ff5706fc2828b235fbaab67637a240
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-10-10 16:47:43 +0200

    exercise 1 finish

--- src/Esercizio1/EsercizioUno.java
+++ src/Esercizio1/EsercizioUno.java
@@ -1,27 +1,40 @@
 package Esercizio1;
 
 import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Set;
 
 public class EsercizioUno {
     public static void main(String[] args) {
 
-        Set<String> setString = new HashSet<String>();
-        //HashSet<String> hashString = new HashSet<String>();
+        Set<String> setPeroleDistinte = new HashSet<String>();
+        Set<String> setParoleDuplicate = new HashSet<String>();
         Scanner input = new Scanner(System.in);
         System.out.println("inserisci un intero che rappresenti il numero di parole da inserire");
-        Integer resp = input.nextInt();
-        for (int i = 0; i < resp ; i++) {
-            System.out.println("inserisci");
-            String arrResp = input.nextLine();
-            if (!arrResp.equals("")) {
-                setString.add(arrResp);
-            }
+        try {
+            Integer resp = input.nextInt();
+            input.nextLine();
+            for (int i = 0; i < resp ; i++) {
+                String arrResp = input.nextLine();
 
+                if (!setPeroleDistinte.add(arrResp)) {
+                    setParoleDuplicate.add(arrResp);
+                }
+
+            }
+        }catch (InputMismatchException ex){
+            System.out.println("il valore non è un intero");
         }
-        System.out.println("parole distinte : " + setString.size());
-        for (String a: setString ) {
+
+
+
+        System.out.println("parole duplicate : ");
+        for (String a: setParoleDuplicate) {
+            System.out.println(a);
+        }
+        System.out.println("numero parole distinte : " + setPeroleDistinte.size());
+        for (String a: setPeroleDistinte ) {
             System.out.println(a);
         }
     }
